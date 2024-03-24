4a4bcc0579c06ba92d7bc3a5933ed6f4cfd200b1
Ioann_B <git@github.com:IoannBL/DZ_2.git>
2024-03-24 22:31:35 +0300

    bot

--- Telebot.py
+++ Telebot.py
@@ -1,4 +1,5 @@
-from telebot import TeleBot
+import asyncio
+from telebot.async_telebot import AsyncTeleBot
 from secret import TOKEN
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
@@ -8,46 +9,68 @@ from selenium.webdriver.chrome.options import Options
 import undetected_chromedriver as undetected
 import time
 import re
+import os
+# current_directory = os.path.dirname(os.path.abspath(__file__))
 
-bot = TeleBot(TOKEN)
-
+bot = AsyncTeleBot(TOKEN)
 
 class FlatFinderBot:
     def __init__(self, bot):
         self.user_states = {}
         self.bot = bot
         self.driver = None
-        self.chat_id = None  # Добавляем переменную для хранения chat_id
+        self.chat_id = None
+        self.parsing = True
+    async def start_bot(self):
+        await self.bot.polling()
     
-    def start_bot(self):
-        self.bot.polling()
-    
-    def start_message(self, message):
-        self.bot.send_message(message.chat.id,
+    async def stop_parsing(self,message):
+        self.parsing = False
+        await self.bot.send_message(message.chat.id,"Поиск остановлен")
+        
+    async def help_pars(self,message):
+        await self.bot.send_message(message.chat.id,
+                                    "Список команд:"
+                                    "'/start '- начало поиска, "
+                                    "'/stop '- остановка поиска ")
+        
+    async def start_message(self, message):
+        await self.bot.send_message(message.chat.id,
                               "Привет, я помогу подобрать тебе квартиру с нужными параметрами в нужной локации."
-                              "Пожалуйста, введи название станции метро (например, 'Проспект мира'):")
-        # Устанавливаем состояние "ожидание станции метро"
-        self.user_states[message.chat.id] = 'awaiting_location'
-        self.chat_id = message.chat.id  # Сохраняем chat_id
+                              "Пожалуйста, введи название станции метро с большой буквы (например, 'Тестовская'), если локация не важна введи '1':")
+        self.user_states[message.chat.id] = 'location'
+        self.chat_id = message.chat.id
     
-    # Остальные методы остаются без изменений
-    def get_location(self, message):
+    async def get_location(self, message):
         location = message.text
-        self.bot.send_message(message.chat.id,
-                              "Спасибо! Теперь введи максимальную стоимость квартиры (например, '65000'):")
-        # Устанавливаем состояние "ожидание стоимости квартиры"
-        self.user_states[message.chat.id] = 'awaiting_price'
+        with open('verification_file.txt', 'r', encoding='utf-8') as file:
+            metro_stations = file.readlines()
+            metro_stations = [station.strip() for station in metro_stations]
+        
+        if location.lower() not in [station.strip().lower() for station in metro_stations]:
+            await self.bot.send_message(message.chat.id,
+                                        "Введенная станция метро не найдена. Пожалуйста, введите корректное название станции метро:")
+            return
+        
+        await self.bot.send_message(message.chat.id,
+                                    "Спасибо! Теперь введите максимальную стоимость квартиры (например, '65000'):")
+        self.user_states[message.chat.id] = 'price'
         self.location = location
-    def get_price(self, message):
+    async def get_price(self, message):
         price = message.text
-        self.bot.send_message(message.chat.id,
+        if not price.isdigit():
+            await self.bot.send_message(message.chat.id,
+                                        "Используйте при вводе стоимости квартиры только цифры. Попробуйте снова.")
+            return
+        await self.bot.send_message(message.chat.id,
                               f"Спасибо! Обрабатываю данные... Ищу квартиры рядом с метро {self.location} не дороже {price} рублей")
-        # Вызываем функцию парсинга с указанными параметрами
-        self.parse_flats(int(price), self.location)
-    def parse_flats(self, max_price, location):
+        self.user_states[message.chat.id] = 'stop_pars'
+        await self.parse_flats(int(price), self.location)
+    async def parse_flats(self, max_price, location):
         options = Options()
         options.add_argument('--headless')
-        options.add_argument('--disable-gpu')
+        # options.add_argument('--disable-gpu')
+        
         service = Service(ChromeDriverManager().install())
         self.driver = undetected.Chrome(service=service)
         
@@ -64,10 +87,11 @@ class FlatFinderBot:
                 price = flat.find_element(By.CLASS_NAME, "_93444fe79c--container--aWzpE").text
                 price_desc = flat.find_element(By.CSS_SELECTOR, '[data-mark="PriceInfo"]').text
                 digits = re.sub(r'\D', '', price)
-                if int(digits) <= max_price and location in loc:
-                    self.bot.send_message(self.chat_id,
-                                          f"{href}\n{desc}\n{loc}\n{price}\n{price_desc}\n")  # Используем chat_id для отправки сообщения
-            
+                if int(digits) <= max_price and (location.lower() in loc.lower() or location == "1"):
+                    await self.bot.send_message(self.chat_id,
+                                          f"{href}\n{desc}\n{loc}\n{price}\n{price_desc}\n")
+            if not self.parsing:
+                break
             try:
                 element = self.driver.find_element(By.XPATH, '//*[@id="frontend-serp"]/div/div[7]/div[2]/button[2]')
                 self.driver.execute_script("arguments[0].scrollIntoView();", element)
@@ -77,22 +101,30 @@ class FlatFinderBot:
                 time.sleep(2)
             except NoSuchElementException:
                 break
-        
+                
         self.driver.quit()
 
         
 flat_finder_bot = FlatFinderBot(bot)
 @bot.message_handler(commands=['start'])
-def start_message(message):
-    flat_finder_bot.start_message(message)
+async def start_message(message):
+    await flat_finder_bot.start_message(message)
 
-@bot.message_handler(func=lambda message: flat_finder_bot.user_states.get(message.chat.id) == 'awaiting_location')
-def get_location(message):
-    flat_finder_bot.get_location(message)
+@bot.message_handler(func=lambda message: flat_finder_bot.user_states.get(message.chat.id) == 'location')
+async def get_location(message):
+    await flat_finder_bot.get_location(message)
 
-@bot.message_handler(func=lambda message: flat_finder_bot.user_states.get(message.chat.id) == 'awaiting_price')
-def get_price(message):
-    flat_finder_bot.get_price(message)
+@bot.message_handler(func=lambda message: flat_finder_bot.user_states.get(message.chat.id) == 'price')
+async def get_price(message):
+    await flat_finder_bot.get_price(message)
+    
+@bot.message_handler(commands=['stop'],func=lambda message: flat_finder_bot.user_states.get(message.chat.id) == 'stop_pars')
+async def stop_parsing(message):
+    await flat_finder_bot.stop_parsing(message)
+    
+@bot.message_handler(commands=['help'])
+async def stop_parsing(message):
+    await flat_finder_bot.help_pars(message)
 
 if __name__ == "__main__":
-    flat_finder_bot.start_bot()
+    asyncio.run(flat_finder_bot.start_bot())
